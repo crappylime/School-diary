24fcdc22ff3d47d6bbe182de7971f49955218885
Marta Sajdak <crappylime@op.pl>
2016-12-17 15:40:23 +0100

    Expose models, complete routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :students do
+
+  root 'visitors#index'
+
+  get '/reports', to: 'reports#subjects', as: 'report_subjects'
+
+  resources :students, :teachers do
     get :subjects
   end
 end
