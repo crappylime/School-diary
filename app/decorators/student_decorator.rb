cbd05090206c9b4a732e74151e6836fcf37c51bf
Marta Sajdak <crappylime@op.pl>
2016-12-18 14:00:38 +0100

    Add value column to subject_item, define avg_notes

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,7 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item.subject_item_notes.map(&:value)
+    notes.length == 0? '0.00' : "%.2f" % (notes.reduce(:+) / notes.length.to_f)
   end
 end
